fcba1ec41897928999aae59e675f49f492d6319b
Jack L <Jack L>
2016-10-05 06:44:07 -0700

    about text is now part of abt section in DOM with video (introduced some issues, now has to be translated on Y axis), prepping for section-height-based animation stuff

--- scripts/keyframes.js
+++ scripts/keyframes.js
@@ -4,19 +4,24 @@
 var rangeEnd = 200
 var ranges = [
 	{
-		rg: [0,40], //range should be able to be set
+		rg: [0,35], //range should be able to be set
 		//as a % of (maybe dynamic pixel value
 		// that changes with resize?)
+
+		/*
+			eg, by the 50% through or the end of the about section, 
+			the text and face icon should be in these positions / opacities
+		*/
 		objs: [
 			{
 				'target': 'jackleng',
-				'translateX': '-75%',
-				'translateY': '1rem'
+				'translateX': '-82.5%',
+				'translateY': '400%'
 			},
 			{
 				'target': 'helloiam',
-				'translateX': '-30%',
-				'translateY': '1rem'
+				'translateX': '-55%',
+				'translateY': '950%'
 			},
 			{
 				'target': 'abtme',
